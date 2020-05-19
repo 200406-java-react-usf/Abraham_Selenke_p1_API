07e222821c558ff70ad8309461094fb5190a9d6d
Abraham Selenke <slinky@Abraham.local>
2020-05-19 16:37:15 -0600

    cheanged role to roles

--- src/middleware/emp-middleware.ts
+++ src/middleware/emp-middleware.ts
@@ -5,7 +5,7 @@ export const empGuard = (req: Request, resp: Response, next) => {
     
     if(!req.session.principal) {
         resp.status(401).json(new AuthenticationError('No session found, please login.'));
-    } else if (req.session.principal.role === 'Employee') {
+    } else if (req.session.principal.roles === 'Employee') {
         next();
     } else {
         resp.status(403).json(new AuthorizationError());
--- src/middleware/manager-middleware.ts
+++ src/middleware/manager-middleware.ts
@@ -5,7 +5,7 @@ export const managerGuard = (req: Request, resp: Response, next) => {
     
     if(!req.session.principal) {
         resp.status(401).json(new AuthenticationError('No session found, please login.'));
-    } else if (req.session.principal.role === 'Manager') {
+    } else if (req.session.principal.roles === 'Manager') {
         next();
     } else {
         resp.status(403).json(new AuthorizationError());
